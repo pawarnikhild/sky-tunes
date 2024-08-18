3adcc64cc86879229a1014c66c08068e8bd2ca21
Pawar Nikhil <pawarnikhild@gmail.com>
2024-08-18 16:52:31 +0530

    Add Discover section layout and styling

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,34 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+const Discover = () => {
+  //   console.log(genres);
+  const genreTitle = "POP";
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center flex-col sm:flex-row mt-4 mb-10">
+        <h2 className="text-left font-bold text-3xl text-white">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none mt-5 sm:mt-0"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard i={i} key={song.key} song={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
